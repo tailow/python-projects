7a6a567e4a0f4e662b0aac933ccf35dfaea368b8
tailow-laptop <revitate@hotmail.com>
2019-01-11 13:15:20 +0200

    sd

--- jari.py
+++ jari.py
@@ -1,94 +1,53 @@
-# CONNECT FOUR
+# CONNECT 4 BOT
 
-import jari
-import jari2
-
-board_width = 7
-board_height = 6
-
-jari1_wins = 0
-jari2_wins = 0
+import random
+import copy
 
 
-def make_board(width, height):
+def calculate_move(board):
+    analysis_board = copy.deepcopy(board)
 
-    new_list = []
+    board_width = len(board[0])
 
-    for y in range(height):
-        new_list.append([])
-        for x in range(width):
-            new_list[y].append(0)
+    legal_moves = []
 
-    return new_list
+    # Check legal moves
+    for i in range(board_width + 1):
+        if is_legal(i, analysis_board):
+            legal_moves.append(i)
 
+    # Check for winning moves
+    if is_checkmate(2, analysis_board, board):
+        return is_checkmate(2, analysis_board, board)
 
-def print_board():
+    # Check for opponent winning moves
+    if is_checkmate(1, analysis_board, board):
+        return is_checkmate(1, analysis_board, board)
 
-    print()
+    if is_legal(4, board):
+        return 4
 
-    for y in range(board_height):
-        for x in range(board_width):
-
-            if board[y][x] == 0:
-                print("_ ", end="")
-
-            elif board[y][x] == 1:
-                print("O ", end="")
-
-            else:
-                print("X ", end="")
-
-        print("\n")
-
-    print("1 2 3 4 5 6 7")
-
-    print()
+    else:
+        return random.choice(legal_moves)
 
 
-def play_game():
-    while True:
-        play_move(1, jari.calculate_move(board))
+def analyse_move(player, column, analysis_board, board):
 
-        if check_win(board):
-            print("jari won!")
-
-            global jari1_wins
-            jari1_wins += 1
-
-            return
-
-        if is_tied(board):
-            print("It's a tie!")
-            return
-
-        play_move(2, jari2.calculate_move(board))
-
-        if check_win(board):
-            print("jari2 won!")
-
-            global jari2_wins
-            jari2_wins += 1
-
-            return
-
-        if is_tied(board):
-            print("It's a tie!")
-            return
-
-
-def play_move(player, column):
+    board_height = len(board)
 
     # For each row, check if there is a piece
     for row in range(board_height):
 
         # If there is, add a piece to the row above
-        if board[row][column - 1] != 0:
-            board[row - 1][column - 1] = player
+        if analysis_board[row][column - 1] != 0:
+            analysis_board[row - 1][column - 1] = player
             break
 
         # If you reach the bottom of the row, add a piece there
         elif row == board_height - 1:
-            board[row][column - 1] = player
+            analysis_board[row][column - 1] = player
+
+    return analysis_board
 
 
 def is_legal(move, board_state):
@@ -104,24 +63,30 @@ def is_legal(move, board_state):
         return True
 
 
-def is_tied(board_state):
+def is_checkmate(player, analysis_board, board):
+    board_width = len(board[0])
 
     legal_moves = []
 
     # Check legal moves
     for i in range(board_width + 1):
-        if is_legal(i, board_state):
+        if is_legal(i, analysis_board):
             legal_moves.append(i)
 
-    if len(legal_moves) == 0:
-        return True
+    # For each move, check if there is a win
+    for move in legal_moves:
+        analysis_board = copy.deepcopy(board)
 
-    else:
-        return False
+        # Play a move in the analysis board and check if the game is over
+        if check_win(analyse_move(player, move, analysis_board, board)):
+            return move
 
 
 def check_win(board_state):
     piece_count = 1
+
+    board_width = len(board_state[0])
+    board_height = len(board_state)
 
     # Check horizontal rows
     for y in range(board_height):
@@ -166,12 +131,3 @@ def check_win(board_state):
                     and board_state[y][x] != 0:
 
                 return board_state[y][x]
-
-
-board = make_board(board_width, board_height)
-
-for i in range(100):
-    play_game()
-
-print("jari1 wins: " + str(jari1_wins))
-print("jari2 wins: " + str(jari2_wins))
